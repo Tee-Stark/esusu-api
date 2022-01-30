fcb07424487c856a96fc607e5d1c6ee44d6e8d53
Timmy Omolana <timmieomolanakiel@gmail.com>
2022-01-30 01:53:40 +0100

    fix infinity bug

--- utils/saveMoney.js
+++ utils/saveMoney.js
@@ -2,21 +2,29 @@ const UserModel = require("../model/User");
 const GroupModel = require("../model/Group");
 const logger = require("../config/logger");
 
-const saveMoney = async (amt, groupId) => {
+const saveMoney = async (amount, groupId) => {
   try {
     const group = await GroupModel.findById(groupId);
     if (group) {
-      const groupMembers = await UserModel.find( { group: groupId } );
-      for(member of groupMembers) {
-        member.amountSaved += amt;
-        await UserModel.findByIdAndUpdate(member._id, member);
-        const saved = await UserModel.find(member._id);
+      const groupMembers = await UserModel.find({ group: groupId });
+      for (member of groupMembers) {
+        if(member.amountSaved == Infinity || typeof member.amountSaved !== "number") {
+          member.amountSaved = 0;
+          logger.info(member.amountSaved);
+        }
+        const updatedSaving = parseInt(Number(member.amountSaved) + Number(amount));
+        console.log(updatedSaving)
+        const saved = await UserModel.findByIdAndUpdate(
+          member._id,
+          { $set: { amountSaved: updatedSaving } },
+          { new: true }
+        );
         if (!saved) {
           throw new Error("Unable to save 5k for this member!");
           return false;
         }
-        logger.info(`Saved ${amt} for ${member.name}`);
-      };
+        logger.info(`Saved ${amount} for ${member.name}`);
+      }
       return true;
     }
   } catch (err) {
@@ -32,7 +40,9 @@ exports.saveForAllGroups = async (amt) => {
         saveMoney(amt, group._id);
         logger.info(`Saved ${amt} for ${group.groupName}`);
       });
-      logger.info("Save new 5K for all group members successfully completed!!!");
+      logger.info(
+        "Save new 5K for all group members successfully completed!!!"
+      );
       return true;
     }
   } catch (err) {
